5cd84da0314701610dd54fbc58ede54bcbdfe104
MarceloBellato <bellato.consultor@gmail.com>
2024-10-16 00:32:23 -0300

    inclusao classe repository

---
+++ src/main/java/com/github/marcelo/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package com.github.marcelo.repository;
+
+import com.github.marcelo.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        // acessa base e salvar o cliente
+    }
+}
