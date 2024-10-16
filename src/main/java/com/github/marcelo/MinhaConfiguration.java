d50f919538e555b1ffcfd4b4e308e28114251414
MarceloBellato <bellato.consultor@gmail.com>
2024-10-16 00:33:04 -0300

    alteracao classe config e application

--- src/main/java/com/github/marcelo/MinhaConfiguration.java
+++ src/main/java/com/github/marcelo/MinhaConfiguration.java
@@ -1,13 +1,9 @@
 package com.github.marcelo;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
-    }
+
 }
